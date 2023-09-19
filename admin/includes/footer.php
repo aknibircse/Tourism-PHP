6a299646553c21d5e2c5a602e0f1acd0d4b6d723
aknibir <aknibir.cse@gmail.com>
2023-09-19 14:19:29 +0600

    Codebase-Updated

---
+++ admin/includes/footer.php
@@ -0,0 +1,9 @@
+ <footer class="sticky-footer bg-white">
+ 	<div class="container my-auto">
+ 		<div class="copyright text-center my-auto">
+ 			<span>&copy; Copyright <script> document.write(new Date().getFullYear()); </script> - Developed by
+ 				<b>Arnob</b>
+ 			</span>
+ 		</div>
+ 	</div>
+ </footer>
